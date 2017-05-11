260be468a830b27f1e0ffc95e7b8f358ac29f4ef
Taylor Ackley <taylorackley@gmail.com>
2017-05-11 14:28:34 -0700

    ready for testing

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 'use strict';
+require('dotenv').config();
 
 let Compiler = require('sass-template-factory');
 
-exports.compiler = function(event, context, callback) {
+exports.compiler = function (event, context, callback) {
 
-let options = {
-    tpl: event.tpl, // Not needed if you plan to grab the template from S3
-    opts: event.opts
+	let options = {
+		tpl: Buffer.from(event.tpl, 'base64').toString('utf-8'),
+		opts: event.opts
+	};
+
+	let c = new Compiler(options);
+
+	c.compile()
+		.then((res) => {
+			callback(null, res);
+		});
+
 };
-
-let c = new Compiler(options);
-
-c.compile()
-	.then((res) => {
-		callback(null, res);
-	})
-
-
-
-}
